16aa24cad40f5bf0d6b80d306fe3de2145cdaa17
yamasaki <yamasakipann0218@gmail.com>
2023-03-13 11:54:45 +0900

    init

--- logfind_test.go
+++ logfind_test.go
@@ -4,12 +4,11 @@ import (
 	"testing"
 
 	"github.com/gostaticanalysis/testutil"
-	"github.com/seipan/logfind"
 	"golang.org/x/tools/go/analysis/analysistest"
 )
 
 // TestAnalyzer is a test for Analyzer.
 func TestAnalyzer(t *testing.T) {
 	testdata := testutil.WithModules(t, analysistest.TestData(), nil)
-	analysistest.Run(t, testdata, logfind.Analyzer, "a")
+	analysistest.Run(t, testdata, Analyzer, "a")
 }
